00c2b91fa859202bfd0ac3588c698626d46e842f
fkakatie <sextonskatie@gmail.com>
2020-08-11 17:34:44 -0600

    add clear btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-    console.log(`script.js loaded!`);
+// VARIABLES FOR ELEMENTS ON PAGE
+const $submitBtn = document.getElementById(`submit-btn`);
+const $clearBtn = document.getElementById(`clear-btn`);
+const $guestNameInput = document.getElementById(`guest-name`);
+const $previousGuestsDiv = document.getElementById(`previous-guests`);
+
+// FUNCTION TO RUN ON DOM LOAD
+document.addEventListener(`DOMContentLoaded`, function() {
+    // FUNCTION TO RUN ON FORM SUBMISSION
+    $submitBtn.addEventListener(`click`, function(e) {
+        e.preventDefault(); // prevent default reload
+        console.log($guestNameInput.value);
+    });
+
+    $clearBtn.addEventListener(`click`, clearGuestBook);
 })
